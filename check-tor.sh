da772cd8acb124debe19a5ebe50df24828105d38
hmdhszd <hmdhszd@gmail.com>
2021-05-08 16:41:01 +0430

    added a bash program

---
+++ check-tor.sh
@@ -0,0 +1,19 @@
+
+while [ 1 ]
+do
+
+sleep 30
+
+x=`curl --max-time 5 --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 -o 1 | xargs`
+
+
+if [ $x = "1" ]
+
+then
+	echo "tor is ok"
+else
+	echo "OMG tor is not working "
+	systemctl restart tor
+fi
+
+done
